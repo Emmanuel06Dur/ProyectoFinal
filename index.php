e39c24ba9c0302792c0c7394fc75fdb6d454e168
ElPedrangas03 <toniramos2345@gmail.com>
2020-11-28 16:00:12 -0600

    Agregamos la seccion para poder ingresarse

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -41,10 +41,7 @@
                     <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Iniciar Sesión</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Registrarse</a>
+                    <a class="nav-link" href="#iniciarsesionaqui">Iniciar Sesión</a>
                 </li>
                 <!-- Esta liga es temporal -->
                 <li class="nav-item">
@@ -64,5 +61,33 @@
             </form>
         </div>
     </nav>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <a name="iniciarsesionaqui"></a>
+    <section class="iniciarsesion" id="inisi">
+        <div id="parallax-inise">
+            <div class="opacidad">
+                <div id="texto">
+                    <form method="post" id="IntentarIngresar">
+                        <a>Iniciar sesion</a>
+
+                        <div class="wrap-input100">
+                            <input class="input100" type="text" id="usuario-ini" name="usuario-ini" placeholder="Usuario">
+                        </div>
+                        <div class="wrap-input100">
+                            <input class="input100" type="password" id="contraseña-ini" name="contraseña-ini" placeholder="Contraseña">
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-success" id="button-submit">Ingresar</button>
+                    </form>
+                </div>
+            </div>
+         </div>
+    </section>
+
+    <!-- Codigos JS -->
+    <script src="javascript/validar-ingresar.js"></script>
 </body>
 </html>
